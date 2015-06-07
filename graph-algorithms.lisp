d05c1e7bb84c2cd9fe2766bf902d2553d4073805
Fabricio Rosario <f@cp300.org>
2015-06-07 15:54:16 -0300

    Removed unused file; added copyright information to new files.

--- graph-algorithms.lisp
+++ graph-algorithms.lisp
@@ -1,3 +1,10 @@
+;; -*- mode: common-lisp -*-
+
+;; Copyright (c) 2015 The OpenWordNet-PT project
+;; This program and the accompanying materials are made available
+;; under the terms of the MIT License which accompanies this
+;; distribution (see LICENSE)
+
 (in-package :cl-wnbrowser)
 
 (defparameter *relations* '(
--- prototypes.lisp
+++
@@ -1,9 +0,0 @@
-;; -*- mode: common-lisp -*-
-
-;; Copyright (c) 2015 The OpenWordNet-PT project
-;; This program and the accompanying materials are made available
-;; under the terms of the MIT License which accompanies this
-;; distribution (see LICENSE)
-
-(in-package :cl-wnbrowser)
-
